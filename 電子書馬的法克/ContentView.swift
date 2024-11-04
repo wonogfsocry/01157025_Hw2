e8bbb3172cd97afe5c2b91df593b88545b12c3dc
wonogfsocry <yangneil930406@gmail.com>
2024-11-04 13:30:48 +0800

    Initial Commit

---
+++ 電子書馬的法克/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  電子書馬的法克
+//
+//  Created by user10 on 2024/11/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 電子書馬的法克/_______App.swift
@@ -0,0 +1,17 @@
+//
+//  _______App.swift
+//  電子書馬的法克
+//
+//  Created by user10 on 2024/11/4.
+//
+
+import SwiftUI
+
+@main
+struct _______App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
