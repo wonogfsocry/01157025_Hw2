6d6e67452061a5f3a5e9d3f343c608c50ba32a45
wonogfsocry <yangneil930406@gmail.com>
2024-11-08 18:40:16 +0800

    wonogfsocry

---
+++ 電子書馬的法克/Detail1.swift
@@ -0,0 +1,7 @@
+//
+//  Untitled.swift
+//  電子書馬的法克
+//
+//  Created by user10 on 2024/11/5.
+//
+
---
+++ 電子書馬的法克/InfoRow.swift
@@ -0,0 +1,18 @@
+//
+//  InfoRow.swift
+//  電子書馬的法克
+//
+//  Created by user10 on 2024/11/5.
+//
+
+import SwiftUI
+
+struct InfoRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InfoRow()
+}
